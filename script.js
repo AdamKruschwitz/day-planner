13cbaff156bd3ac441c30f1a70a54df665411d98
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-09-30 15:04:05 -0700

    added script.js with pseudocode and linked

---
+++ script.js
@@ -0,0 +1,10 @@
+// PSEUDOCODE =====================================================================================================
+// Add rows for 9-5 work hours to #container div
+//    Each row should have a text input field for the user to add text 
+//        If there is data saved for this text field, it should be added to the page.
+//    Each row should have a save button to save data from text input
+//    The timeblocks main input field should be colored depending on whether it's a current, past, or future hour.
+//    When the save button is clicked...
+//        The value of the input field should be saved in local storage
+// ================================================================================================================
+
