21d7ca1a22c71db6c08572a18ea89972e63d41c6
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-09-30 15:11:41 -0700

    added barebones code with pseudocode comments

--- script.js
+++ script.js
@@ -8,3 +8,23 @@
 //        The value of the input field should be saved in local storage
 // ================================================================================================================
 
+var hours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+var container = $(".container");
+
+for(var hour in hours) {
+    // Create the hour element
+    let hourEl = $("<section>");
+    hourEl.class = "row w-100";
+
+    // Create a label for that element
+
+    // Create a text input field for that element
+
+    // If there is data saved, populate the data for this element
+
+    // Check the time, and set the color of this text input accordingly
+
+    // Create a save button
+
+    // Assign an on click event which saves the text input info to local storage
+}
